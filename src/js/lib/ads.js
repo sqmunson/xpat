c3f3fb952323390c7800e1b691387a5631785c98
Scott Munson <scott@thinkcontra.com>
2015-06-08 19:34:48 -0400

    use new pubgears script with cb=8778 query param, not really async tags still

--- src/js/lib/ads.js
+++ src/js/lib/ads.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($, p) {
 
 	window.loadAds = function() {
 		var width = window.innerWidth,
+		adspots_300x50,
 		adspots_300x250,
 		adspots_300x600,
 		adspots_728x90;
@@ -10,12 +11,23 @@ jQuery(document).ready(function($, p) {
 
 	    if (width < 728) {
 	    	// smaller than 728px screen
+	    	adspots_300x50 = $('.mobile-300x50');
 	    	adspots_300x250 = $('.mobile-300x250');
+
+	    	if (adspots_300x50.length) {
+	    		adspots_300x50.each(function() {
+	    			if (this.childNodes.length === 0 && !$(this).hasClass('loaded')) {
+	    				$(this).addClass('loaded');
+				        p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatrow/ros/300x50?cb=' + Math.round(Math.random() * 100000) + '"></script><div class="space"></div>');
+	    			}
+	    		});
+	    	}
+
 	    	if (adspots_300x250.length) {
 	    		adspots_300x250.each(function() {
 	    			if (this.childNodes.length === 0 && !$(this).hasClass('loaded')) {
 	    				$(this).addClass('loaded');
-				        p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatus/ros/300x250"></script><div class="space"></div>');
+				        p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatrow/ros/300x250?cb=' + Math.round(Math.random() * 100000) + '"></script><div class="space"></div>');
 	    			}
 	    		});
 	    	}
@@ -29,7 +41,7 @@ jQuery(document).ready(function($, p) {
 	    		adspots_300x250.each(function() {
 	    			if (this.childNodes.length === 0 && !$(this).hasClass('loaded')) {
 	    				$(this).addClass('loaded');
-				    	p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatus/ros/300x250"></script><div class="space"></div>');
+				    	p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatus/ros/300x250?cb=' + Math.round(Math.random() * 100000) + '"></script><div class="space"></div>');
 	    			}
 	    		});
 	    	}
@@ -38,7 +50,7 @@ jQuery(document).ready(function($, p) {
 	    		adspots_300x600.each(function() {
 	    			if (this.childNodes.length === 0 && !$(this).hasClass('loaded')) {
 	    				$(this).addClass('loaded');
-			    		p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatus/ros/300x600"></script><div class="space"></div>');
+			    		p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatus/ros/300x600?cb=' + Math.round(Math.random() * 100000) + '"></script><div class="space"></div>');
 	    			}
 	    		});
 	    	}
@@ -47,7 +59,7 @@ jQuery(document).ready(function($, p) {
 	    		adspots_728x90.each(function() {
 	    			if (this.childNodes.length === 0 && !$(this).hasClass('loaded')) {
 	    				$(this).addClass('loaded');
-				    	p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatus/ros/728x90"></script><div class="space"></div>');
+				    	p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatus/ros/728x90?cb=' + Math.round(Math.random() * 100000) + '"></script><div class="space"></div>');
 	    			}
 	    		});
 	    	}
