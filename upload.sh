a59ee944984e0eda65cf68ec09defc2f54ae9a76
Scott Munson <sqmunson@gmail.com>
2015-09-17 13:51:25 -0400

    update aws url for uploading

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@
 # secure copy the zip folder from the local MAMP theme directory to the home folder on the server
 # must ssh into server and run deploy.sh after uploading
 
-scp -i ~/.ssh/xpat.pem /Applications/MAMP/htdocs/xpatnation/wp-content/themes/src.zip bitnami@ec2-52-1-149-57.compute-1.amazonaws.com:/home/bitnami/
+scp -i ~/.ssh/xpat.pem /Applications/MAMP/htdocs/xpatnation/wp-content/themes/src.zip bitnami@ec2-52-5-252-81.compute-1.amazonaws.com:/home/bitnami/
