80f64f749a0dee30923588244cc09efdc6f66f16
Scott Munson <sqmunson@gmail.com>
2015-12-09 12:13:41 -0500

    remove old 300x250 tags, add Genesis code to ads.js

--- src/js/lib/ads.js
+++ src/js/lib/ads.js
@@ -27,10 +27,7 @@ jQuery(document).ready(function($, p) {
 	    		adspots_300x250.each(function() {
 	    			if (this.childNodes.length === 0 && !$(this).hasClass('loaded')) {
 	    				$(this).addClass('loaded');
-				        p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xptnt2/ros/300x250?cb=' + Math.round(Math.random() * 100000) + '&domain=' + window.location.hostname + '"></script><div class="space"></div>');
-				        // p($(this), '<script type="text/javascript">kmn_placement = "102df5c44f5f8da4eed0736e427d5df6";</script><script type="text/javascript" src="//cdn.komoona.com/scripts/kmn_sa.js"></script><div class="space"></div>');
-				        // $(this).append('<script type="text/javascript">kmn_placement = "102df5c44f5f8da4eed0736e427d5df6";</script><script type="text/javascript" src="//cdn.komoona.com/scripts/kmn_sa.js"></script><div class="space"></div>');
-						// p($(this), '<div id="kmni_102df5c44f5f8da4eed0736e427d5df6"></div><script type="text/javascript" src="//cdn.komoona.com/scripts/kmn_sa.js" async></script><script type="text/javascript"> var kmn_tags = window.kmn_tags || []; kmn_tags.push("102df5c44f5f8da4eed0736e427d5df6"); </script><div class="space"></div>');
+				        p($(this), '<script type="text/javascript" src="http://tags.pubgears.com/xpatrow/ros/300x250?cb=' + Math.round(Math.random() * 100000) + '"></script><div class="space"></div>');
 	    			}
 	    		});
 	    	}
@@ -69,5 +66,18 @@ jQuery(document).ready(function($, p) {
 	    	}
 	    }
 	};
+
+	// load genesis 320x50 ad
+	if (window.innerWidth <= 800 && window.innerHeight <= 600 && window.location.pathname.indexOf('can-the-saudis-sink-putin') > -1) {
+		// add tracker to <head>
+	    var script = document.createElement('script');
+	    script.src = "http://adgenesis.s3.amazonaws.com/mobile/data.min.js";
+	    document.getElementsByTagName('head')[0].appendChild(script);
+
+	    // insert pubgears tag into DOM
+	    var div = document.createElement('div');
+	    document.getElementsByTagName('body')[0].appendChild(div);
+        p($(div), '<script type="text/javascript" src="http://tags.pubgears.com/xptnt2/ros/320x50?cb=' + Math.round(Math.random() * 100000) + '&domain=' + window.location.hostname + '"></script>');
+	}
 
 }(jQuery, postscribe));
