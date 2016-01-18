9ab2704ca973318b305a41500e3a6407ece51c1c
Scott Munson <sqmunson@gmail.com>
2016-01-18 10:53:52 -0500

    add ssh comment

--- upload.sh
+++ upload.sh
@@ -3,4 +3,6 @@
 # secure copy the zip folder from the local MAMP theme directory to the home folder on the server
 # must ssh into server and run deploy.sh after uploading
 
+# ssh -i ~/ssh/xpat.pem bitnami@ec2-52-5-252-81.compute-1.amazonaws.com
+
 scp -i ~/.ssh/xpat.pem /Applications/MAMP/htdocs/xpatnation/wp-content/themes/src.zip bitnami@ec2-52-5-252-81.compute-1.amazonaws.com:/home/bitnami/
