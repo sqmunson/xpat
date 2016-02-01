7d59e77034829d67c34f5f8c81865cfc25e049b3
Scott Munson <sqmunson@gmail.com>
2016-02-01 14:26:28 -0500

    add facebook pixel tracker code to <head>

--- src/header.php
+++ src/header.php
@@ -30,6 +30,7 @@
                 f.parentNode.insertBefore(e, f);
             }(document.createElement('script'), document.getElementsByTagName('script')[0], '//cdn.taboola.com/libtrc/xpatnation/loader.js');
         </script>
+
 		<script>
         // conditionizr.com
         // configure environment tests
@@ -38,6 +39,20 @@
             tests: {}
         });
         </script>
+
+        <!-- Facebook Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1535361346713989');
+            fbq('track', "PageView");
+        </script>
+        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1535361346713989&ev=PageView&noscript=1"/></noscript>
+        <!-- End Facebook Pixel Code -->
 	</head>
 	<body <?php body_class(); ?>>
 
